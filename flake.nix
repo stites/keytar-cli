79d3d1faa6b8df79583567154fb9f871b2950f03
Sam Stites <stites@users.noreply.github.com>
2022-02-15 09:55:56 -0500

    add update script

--- flake.nix
+++ flake.nix
@@ -2,15 +2,26 @@
   description = "keytar";
 
   inputs.utils.url = "github:numtide/flake-utils";
+  inputs.devshell.url = "github:numtide/devshell";
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs, utils, devshell }:
+    { overlay = final: prev: {
+        keytar-cli = (prev.callPackage ./. { pkgs=prev; }).package;
+    }; } // (utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+	overlays = [ devshell.overlay ];
+      };
     in {
       defaultPackage = (pkgs.callPackage ./. { inherit pkgs system; }).package;
-    }) // {
-      overlay = final: prev: {
-        keytar-cli = (prev.callPackage ./. { pkgs=prev; }).package;
+      devShell = pkgs.devshell.mkShell {
+        commands = [{
+	  name = "update-nix";
+	  command = ''
+	    ${pkgs.yarn}/bin/yarn install
+	    ${pkgs.yarn2nix}/bin/yarn2nix > ./yarn.nix
+	  '';
+	}];
       };
-    };
+    }));
 }
